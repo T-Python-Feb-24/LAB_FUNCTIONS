a43706f47c475ef03bc000887c815c116810e4f3
Maha <mahaalqarni.offficial@gmail.com>
2024-02-09 04:55:59 +0300

    Main Lab - Done

--- functions.py
+++ functions.py
@@ -1,25 +1,25 @@
-'''
-Create a function that takes 1 parameter of type int , 
-then it prints out the result formatted like the following 
-patter (if we give it 5
-'''
 
 
-def number (num:int) :
- ''' (number) is a function that holds one parameter'''
- for i in range (num, 0, -1):
-    for j in range (i, 0, -1):
-       total = i + j 
-       print(total)
-    
-       print(j)
-    
-number(5) 
+## Create a function that takes 1 parameter of type int , 
+# then it prints out the result formatted like 
+# the following patter (if we give it 5 for example):
 
-'''
-Document the newly created function. 
-describe what it does, then print the documentation. 
-'''
+# 5 4 3 2 1   
+# 4 3 2 1   
+# 3 2 1   
+# 2 1   
+# 1   
+
+
+
+def number(num:int):
+   '''(number) is a function that holds one parameter to print a formatted patern'''
+   for i in range(num, 0, -1):
+      print(" ".join(str(j) for j in range(i, 0, -1)))
+number(5)
+
+
+### Document the newly created function. describe what it does, 
+# then print the documentation. 
 
 print(number.__doc__)
-
