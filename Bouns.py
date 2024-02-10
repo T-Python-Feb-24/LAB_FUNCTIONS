9f78686502b25eb276d41d71ff87fa282bca4e53
waad <waad.f.alanzi@gmail.com>
2024-02-11 00:05:27 +0300

    solve

--- Bouns.py
+++ Bouns.py
@@ -1,15 +1,11 @@
-string="Hey"
-string2=":)"
-string3="Pls"
 
-def strings(x:str):
-
- for i in range(5, 0, -1): # 
-    for m in range(i,0,-1 ):
-        print(x,end=' ')
-    print('\r')
-    
-
-strings(string)
-strings(string2)
-strings(string3)
+def string(x):
+    string=""
+    for i in range(x,0,-1):
+        for m in range(i,0,-1):
+            string +=f"{str(i-m)}" 
+        string += "\n"
+    return string
+        
+result=string(5)
+print(result)
