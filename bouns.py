f658e090285d9884bb315d32a9fd72d66ac70d46
Fai <faiabdulla123@gmail.com>
2024-02-11 19:25:30 +0300

    lap3

--- bouns.py
+++ bouns.py
@@ -1,8 +1,10 @@
 def pattern_string(n):
-    #Returns a pattern of decreasing integers starting from n to 1 as a s Parameters: n (int): The starting integer value.
+    '''Returns a pattern of decreasing integers starting from n to 1 as a s Parameters: n (int): The starting integer value.'''
     pattern=""
     for i in range(n,0,-1):
         for j in range(i,0,-1):
          pattern+=str(j)
     pattern +="\n"
     return pattern 
+pattern= pattern_string(10)
+print(pattern)
--- lap3.py
+++ lap3.py
@@ -1,5 +1,5 @@
 def num_pattern(n:int):
-    #tReturns a pattern of decreasing integers starting from n to 1 as a string. Parameters:n (int): The  integer value.
+    '''Returns a pattern of decreasing integers starting from n to 1 as a string. Parameters:n (int): The  integer value.'''
     for i in range(n,0,-1):
         for j in range(i,0,-1): 
             print(j,end=' ')
